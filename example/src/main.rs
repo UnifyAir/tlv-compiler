963796fe73e966f026c2a5dd7ca4db6b5d21abce
k415hu <kaishusahu101@gmail.com>
2024-12-18 19:09:50 +0530

    minor fixes

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,24 +1,25 @@
 use tlv_derive::{ TlvEncode };
 use tlv::prelude::*;
+use tlv::{Bytes, BytesMut, BufMut};
 fn main() {
 }
 
 #[derive(TlvEncode)]
 #[tlv_config(tag=123, length_bytes_format=4, estimated_size=2048)]
 pub struct Tester{
-    #[tlv_config(tag=123, length_bytes_format=4)]
-    lester: Lester,
+    // #[tlv_config(tag=123, length_bytes_format=4)]
+    // lester: Lester,
     #[tlv_config(tag=123, length_bytes_format=4)]
     sohan: u8
 
 }
 
-#[derive(TlvEncode)]
-#[tlv_config(tag=123, length=3, length_bytes_format=4, estimated_size=2048)]
-pub struct Lester{
-    #[tlv_config(tag=123, length=3, length_bytes_format=4)]
-    mohan: Option<u8>,
-
-}
-
-
+// #[derive(TlvEncode)]
+// #[tlv_config(tag=123, length=3, length_bytes_format=4, estimated_size=2048)]
+// pub struct Lester{
+//     #[tlv_config(tag=123, length=3, length_bytes_format=4)]
+//     mohan: Option<u8>,
+//
+// }
+//
+//
